10850902d5893445f0776425068db64cbff32e4c
Pedro Queiroga <90732961+PedroQueiroga9@users.noreply.github.com>
2025-03-13 13:25:32 -0300

    classe repository criada

---
+++ src/main/java/com/example/pessoa/repository/PessoaRepository.java
@@ -0,0 +1,11 @@
+package com.example.pessoa.repository;
+
+import com.example.pessoa.model.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+
+}
